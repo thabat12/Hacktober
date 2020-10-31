cec31c8f1e542c6004e95f5c6eae6275143bd73d
Sarvesh Sathish <sarveshsathish.2004@gmail.com>
2020-10-31 05:38:07 -0500

    Sarvesh

--- app/src/main/java/com/example/hacktober/activities/MainActivity.kt
+++ app/src/main/java/com/example/hacktober/activities/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.hacktober.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import com.example.hacktober.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_pin.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +16,10 @@ class MainActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
         )
         setContentView(R.layout.activity_main)
+        camera.setOnClickListener {
+            startActivity(
+                    Intent(this@MainActivity, PictureActivity::class.java)
+            )
+        }
     }
 }
