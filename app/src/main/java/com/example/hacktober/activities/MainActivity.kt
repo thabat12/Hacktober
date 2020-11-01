893843e5040a0a71c184584efd9b52604875703f
Sarvesh Sathish <sarveshsathish.2004@gmail.com>
2020-10-31 19:09:22 -0500

    Sarvesh

--- app/src/main/java/com/example/hacktober/activities/MainActivity.kt
+++ app/src/main/java/com/example/hacktober/activities/MainActivity.kt
@@ -4,7 +4,11 @@ import MyAdapter
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.viewpager.widget.ViewPager
 import com.example.hacktober.R
 import com.google.android.material.tabs.TabLayout
@@ -14,6 +18,7 @@ import kotlinx.android.synthetic.main.activity_pin.*
 class MainActivity : AppCompatActivity() {
     lateinit var tabLayout: TabLayout
     lateinit var viewPager: ViewPager
+    var isOpen = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,8 +26,8 @@ class MainActivity : AppCompatActivity() {
         tabLayout = findViewById(R.id.tabLayout)
         viewPager = findViewById(R.id.viewPager)
         tabLayout.addTab(tabLayout.newTab().setText("Private"))
-        tabLayout.addTab(tabLayout.newTab().setText("com.example.hacktober.fragments.Cricket"))
-        tabLayout.addTab(tabLayout.newTab().setText("NBA"))
+        tabLayout.addTab(tabLayout.newTab().setText("Conversations"))
+
         tabLayout.tabGravity = TabLayout.GRAVITY_FILL
         val adapter = MyAdapter(this, supportFragmentManager,
             tabLayout.tabCount)
@@ -35,5 +40,44 @@ class MainActivity : AppCompatActivity() {
             override fun onTabUnselected(tab: TabLayout.Tab) {}
             override fun onTabReselected(tab: TabLayout.Tab) {}
         })
+
+        val fab: View = findViewById(R.id.fab)
+
+
+        val fabOpen = AnimationUtils.loadAnimation(this,R.anim.rotate_open_anim)
+        val fabClose = AnimationUtils.loadAnimation(this,R.anim.rotate_close_anim)
+        val fabRClockwise = AnimationUtils.loadAnimation(this,R.anim.from_bottom_anim)
+        val fabRAntiClockwise = AnimationUtils.loadAnimation(this,R.anim.to_botton_anim)
+        fab.setOnClickListener { view ->
+            if(isOpen)
+            {
+                fab.startAnimation(fabClose)
+                camera.startAnimation(fabClose)
+                camera.visibility = View.INVISIBLE
+                new_album.startAnimation(fabClose)
+                new_album.visibility = View.INVISIBLE
+                photos.startAnimation(fabClose)
+                photos.visibility = View.INVISIBLE
+                fab.startAnimation(fabRAntiClockwise)
+                isOpen = false
+            }
+            else
+            {
+                fab.startAnimation(fabOpen)
+                camera.startAnimation(fabOpen)
+                new_album.startAnimation(fabOpen)
+                photos.startAnimation(fabOpen)
+                fab.startAnimation(fabRClockwise)
+                fab.isClickable
+                camera.isClickable
+                new_album.isClickable
+                photos.isClickable
+                isOpen = true
+            }
+
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/hacktober/activities/PictureActivity.kt
+++ app/src/main/java/com/example/hacktober/activities/PictureActivity.kt
@@ -1,12 +1,49 @@
 package com.example.hacktober.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.hacktober.R
+import java.security.SecureRandom
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
 
 class PictureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picture)
+    }
+
+    @Throws(Exception::class)
+    fun encrypt(toEncrypt: String, key: String): ByteArray? {
+        // create a binary key from the argument key (seed)
+        val sr = SecureRandom(key.toByteArray())
+        val kg: KeyGenerator = KeyGenerator.getInstance("twofish")
+        kg.init(sr)
+        val sk: SecretKey = kg.generateKey()
+
+        // create an instance of cipher
+        val cipher: Cipher = Cipher.getInstance("twofish")
+
+        // initialize the cipher with the key
+        cipher.init(Cipher.ENCRYPT_MODE, sk)
+
+        // enctypt!
+        return cipher.doFinal(toEncrypt.toByteArray())
+    }
+
+    @Throws(Exception::class)
+    fun decrypt(toDecrypt: ByteArray?, key: String): String? {
+        // create a binary key from the argument key (seed)
+        val sr = SecureRandom(key.toByteArray())
+        val kg: KeyGenerator = KeyGenerator.getInstance("twofish")
+        kg.init(sr)
+        val sk: SecretKey = kg.generateKey()
+
+        // do the decryption with that key
+        val cipher: Cipher = Cipher.getInstance("twofish")
+        cipher.init(Cipher.DECRYPT_MODE, sk)
+        val decrypted: ByteArray = cipher.doFinal(toDecrypt)
+        return String(decrypted)
     }
 }
